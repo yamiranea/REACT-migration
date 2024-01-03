2f66c7c323ffb3f9418659561f9619120b36e1eb
yamiranea <yamilarenea@gmail.com>
2024-01-03 02:05:08 +0100

    description page almost finished

--- src/views/DescriptionPage/DescriptionPage.jsx
+++ src/views/DescriptionPage/DescriptionPage.jsx
@@ -4,12 +4,19 @@ import descriptionImage from "../../assets/images/description-1-app.png";
 
 const DescriptionPage = () => {
   return (
-    <div>
-      <img
-        src={descriptionImage}
-        className="w-auto mt-32 md:w-11/12 lg:w-6/12"
-      />
-    </div>
+    <>
+      <div>
+        <img
+          src={descriptionImage}
+          className="w-auto mt-32 md:w-11/12 lg:w-6/12"
+        />
+      </div>
+      <div>
+        <h2 className="text-4xl text-main w-5/6 font-bold mx-auto mt-8">
+          Organize Your Time And Start Getting Results
+        </h2>
+      </div>
+    </>
   );
 };
 
