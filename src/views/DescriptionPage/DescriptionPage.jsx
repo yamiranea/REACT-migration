5f9afec3de1c729f5a75f92749022e63db28f843
yamiranea <yamilarenea@gmail.com>
2024-01-03 01:59:25 +0100

    app jsx modified

--- src/views/DescriptionPage/DescriptionPage.jsx
+++ src/views/DescriptionPage/DescriptionPage.jsx
@@ -5,7 +5,10 @@ import descriptionImage from "../../assets/images/description-1-app.png";
 const DescriptionPage = () => {
   return (
     <div>
-      <img src={descriptionImage} className="w-auto md:w-11/12 lg:w-6/12" />
+      <img
+        src={descriptionImage}
+        className="w-auto mt-32 md:w-11/12 lg:w-6/12"
+      />
     </div>
   );
 };
